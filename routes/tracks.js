50ffa35d84b1d491258da9586df174a82d034c45
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-27 17:40:49 +0200

    How to Upload mp3 Node Express && How to create a Middleware in Node and Express

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const router = express.Router()
+// Middleware validatorCreateItem
+const { validatorCreateItem } = require('../validators/tracks')
 const { getItems, getItem, createItem } = require('../controllers/tracks')
 
 router.get('/', getItems)
-router.post('/', createItem)
+// post request is the
+// validation we are doing when creating a new register
+router.post(
+	'/',
+	// Middleware validatorCerateItem
+	validatorCreateItem,
+	createItem
+)
 module.exports = router
