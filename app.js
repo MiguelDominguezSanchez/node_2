2773c7efa30ee56a5873d0d78a363cd34945050e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-03-31 17:10:09 +0200

    Errors Traceability of Backend with Slack

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const morganBody = require('morgan-body')
+const loggerStream = require('./utils/handlerLogger')
 const dbConnect = require('./config/mongo')
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('storage'))
+
+morganBody(app, {
+	noColors: true,
+	stream: loggerStream,
+	skip: function (req, res) {
+		return res.statusCode < 400 // TODO: 2xx, 3xx
+	},
+})
 
 const port = process.env.PORT || 3000
 
